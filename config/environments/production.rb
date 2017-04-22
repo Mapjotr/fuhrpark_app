14d069441e51e7424940cee77ad267eda52a37a6
Mapjotr <peter.schmiz@gmx.de>
2017-04-22 09:35:50 +0200

    adapt stuff to new url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'evening-cliffs-11899.herokuapp.com'
+  host = 'shrouded-plateau-69470.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
